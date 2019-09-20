324770f6fca7e7431c7ec8735ecdc3f0eaa9e86a
johnhaus <john.haus@outlook.com>
2019-09-19 22:09:57 -0500

    added users/devise gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts 'Cleaning database...'
+Galaxy.destroy_all
+
 puts 'Creating galaxies...'
 galaxy_1 = Galaxy.new(name: "First", description: "First Galaxy")
 galaxy_1.save!
 
 galaxy_2 = Galaxy.new(name: "Second", description: "Second Galaxy")
 galaxy_2.save!
+
+galaxy_3 = Galaxy.new(name: "Third", description: "Third Galaxy")
+galaxy_3.save!
 puts 'Finished!'
