5791c2a40246ce0d532a818d7456c0979349212e
johnhaus <john.haus@outlook.com>
2019-10-19 19:39:18 -0500

    added more testing

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,5 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :firefox
+  driven_by :selenium, using: :firefox # can use :headless_firefox as well
 end
--- test/system/galaxies_test.rb
+++ test/system/galaxies_test.rb
@@ -8,14 +8,14 @@ class GalaxiesTest < ApplicationSystemTestCase
   end
 
   test "lets a signed in user view their rentals" do
-    login_as users(:test_user)
+    login_as users(:test_user1)
     visit galaxies_url
     click_link('My Rentals')
     assert_equal rents_path, page.current_path
   end
 
   test "lets a signed in user create a new rental" do
-    login_as users(:test_user)
+    login_as users(:test_user1)
     visit "/galaxies"
     click_link('Rent', match: :prefer_exact)
 
@@ -24,6 +24,26 @@ class GalaxiesTest < ApplicationSystemTestCase
 
     click_on 'Create Rent'
     assert_text "Details for your rental"
+  end
+
+  test "user can't see or edit another user's rentals" do
+    login_as users(:test_user1)
+    visit "/galaxies"
+    click_link('Rent', match: :prefer_exact)
+
+    fill_in "rent_start_date", with: Time.now + 2.days
+    fill_in "rent_end_date", with: Time.now + 5.days
+
+    click_on 'Create Rent'
+    assert_text "Details for your rental"
+    logout(:test_user1)
+
+    login_as users(:test_user2)
+    visit "/rents/1"
+    assert_equal root_path, page.current_path
+
+    visit "/rents/1/edit"
+    assert_equal root_path, page.current_path
   end
 end
 
